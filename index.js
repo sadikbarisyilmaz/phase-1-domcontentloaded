7ee41a5137130a4f29a92cd0df2c3165b8f763b4
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-12 17:43:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("The DOM has loaded");
+
+    const text = document.querySelector("#text")
+    text.innerHTML = "This is really cool!"
+
+  });
